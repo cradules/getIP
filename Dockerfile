e1e5c81f44df7b4612379318fbc9a348a3545700
cradulescu <seaman105@yahoo.com>
2021-10-02 02:24:34 +0300

    Change tree structure and fix

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM python:slim
 
 WORKDIR /app
 
-RUN pip3 install flask && \
-    pip3 install gunicorn
+RUN pip3 install flask gunicorn
 
-ADD . .
+ADD src/ .
 
 ENTRYPOINT ["gunicorn", "-c", "config.py", "wsgi:app"]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import socket
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -9,6 +9,14 @@ def get_data():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     return hostname, ip_address
+
+
+@app.route('/', methods=['GET'])
+def index():
+    try:
+        return jsonify(get_data())
+    except Exception as e:
+        return jsonify(e)
 
 
 if __name__ == "__main__":
